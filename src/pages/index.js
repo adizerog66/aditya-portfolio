bab286d7faaed000280c24f7b1988ce8c5492056
Aditya Garikapati <aditya.garikapati@gmail.com>
2018-12-14 18:52:57 +0530

    Basic

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,26 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
-    <h1>Portfolio of Aditya Garikapati</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
+
+<div class="side-nav">
+      <a class="active" href="localhost:8000">Home</a>
+      <a href="page-2">Work</a>
+      <a href="page-2">About</a>
+      <a href="page-2">Contact</a>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    
+    <div>
+      <div className="Hero">
+      <div className="heroGroup">
+      <p class="kerned grey"> intro </p>
+      <h1> Hi! I'm <span class="yellow">Aditya</span> <span class="blue">Garikapati</span>, an interaction designer <span class="grey">looking to graduate in <br></br>the summer of 2019. </span></h1>
+      <p class="small grey"> Currently working at my college's incubator sculpting ideas and concepts into prototypes and realizable product strategies. </p>
+      <Link to="/page-2/">View resume <i class="right"></i> </Link>
+      
+      </div>
+     </div>
+    </div>
+
   </Layout>
 )
 
